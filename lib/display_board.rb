2b99d1c2068d67d3f5bea7906b0fc188722eb079
Achsah Abraham <achsahmp@gmail.com>
2018-05-30 21:15:36 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,7 @@
-def display_board
-  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  display_board(board)
-  
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "  
+  puts "-----------"
+  puts "   |   |   "
+end 
